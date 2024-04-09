390088ef05233f318345880e46f28e2d12d37e58
113375 <knyazevdima05@gmail.com>
2024-04-09 22:28:43 +0300

    Надо доделать footer и текст

--- views/pages/about.php
+++ views/pages/about.php
@@ -3,7 +3,4 @@
 <div class="band-img">
 </div>
 
-<div style="height: 1200px; width: 1200px; color: black; z-index: 10; color: white;">
-текста вообще буд-то нет
-</div>
 <script src="/resources/js/about/about.js"></script>
--- views/pages/parts/footer.php
+++ views/pages/parts/footer.php
@@ -1,4 +1,25 @@
+<div class="footer">
+    <div class="footer-text">
+        <div class="block tg">
+            <div class="link tg">
+                https://t.me/zaryavoctyabre
+            </div>
+            <div class="icon tg">
+                <img src="/resources/img/telegram.svg" alt="">
+            </div>
+        </div>
+        <div class="block vk">
+            <div class="link tg">
+                https://vk.com/octoberzarya
+                </div>
+                <div class="icon tg">
+                    <img src="/resources/img/vk.svg" alt="">
+                </div>
+            </div>
+    </div>
 </div>
+</div>
+
 <script src="/resources/js/index.js"></script>
 </body>
 </html>
